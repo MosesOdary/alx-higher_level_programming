0676840bc513919651163fea6434e4049e7e0be1
MosesOdary <mosesodary101@gmail.com>
2024-01-23 03:33:01 +0300

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,10 @@
 import sys
 
 
-
 def safe_function(fct, *args):
     try:
         return fct(*args)
+
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
+        return None
