f9296c4189e8f2f591f7343b3a340d8671068363
MosesOdary <mosesodary101@gmail.com>
2024-01-30 01:28:47 +0300

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,2 +1,43 @@
 #!/usr/bin/python3
 
+"""Class rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Constructor -
+        Args:
+            width (int): Width of the rectangle. Default = 0.
+            height (int): Height of the rectangle. Default = 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """integer: Get rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("Width must be a positive value")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height is not an integer")
+        elif (value < 0):
+            raise ValueError("Height must be a positive value")
+        self.__height = value
