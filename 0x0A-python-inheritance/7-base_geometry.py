91611eac549f15a5a4ef85ca492b56ba84f7340a
MosesOdary <mosesodary101@gmail.com>
2024-02-06 00:46:32 +0300

    pycodestyle fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
     def area(self):
         """Unimplemented function"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validate value"""
         if type(value) is not int:
