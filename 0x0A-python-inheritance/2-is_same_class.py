8560ad8224199e9b9cf322e3b07a021203ab874b
MosesOdary <mosesodary101@gmail.com>
2024-02-05 23:57:30 +0300

    Fixed indentation errors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,10 +2,10 @@
 
 """Instance checker class"""
 def is_same_class(obj, a_class):
-	"""
+    """
         Returns True if the object is exactly an instance of the specified class ; otherwise False
-	"""
-	if type(obj) == a_class:
-		return True
-	
+    """
+    if type(obj) == a_class:
+        return True
+    
     return False
