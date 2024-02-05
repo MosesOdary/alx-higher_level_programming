0f3d8a4cb90339a52c4617ec8a5cad1ef9c1ff8f
MosesOdary <mosesodary101@gmail.com>
2024-02-06 01:38:25 +0300

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
+"""Class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    """
+        Class Rectangle
+    """
+    def __init__(self, width, height):
+        """Constructor"""
+        self.integer_validation("width", width)
+        self.__width = width
+        self.integer_validation("height", height)
+        self.__height = height
